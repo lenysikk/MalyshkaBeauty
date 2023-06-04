c974977f9005a4b9f59fd5e418cd2b472c52859f
arcadka@gmail.com <arcadka@gmail.com>
2023-06-04 20:02:41 +0300

    Initial commit

---
+++ src/main/java/io/proj3ct/MilashkaBeautyBot/MilashkaBeautyBotApplication.java
@@ -0,0 +1,13 @@
+package io.proj3ct.MilashkaBeautyBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilashkaBeautyBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilashkaBeautyBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/proj3ct/MilashkaBeautyBot/MilashkaBeautyBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.proj3ct.MilashkaBeautyBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilashkaBeautyBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
